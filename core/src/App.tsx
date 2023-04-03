813ea96d04e1279422e1d999b4131397946f1a84
eolo-ro <benjamincorderog@gmail.com>
2023-04-02 23:05:24 -0400

    added components to app.tsx

--- core/src/App.tsx
+++ core/src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import SocialSidebar from "./components/socialsidebar/SocialSidebar";
+import About from "./pages/About";
+import Home from "./pages/home/Home";
+import Projects from "./pages/Projects";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <SocialSidebar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
